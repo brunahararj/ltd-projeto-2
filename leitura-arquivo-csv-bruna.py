0475fe0c142ec06016ede176d2153dd0064a19cb
Bruna <140204799+brunahararj@users.noreply.github.com>
2024-05-25 13:54:37 -0300

    Update leitura-arquivo-csv-bruna.py

--- leitura-arquivo-csv-bruna.py
+++ leitura-arquivo-csv-bruna.py
@@ -1,8 +1,8 @@
 import pandas as pd
-data = pd.read_csv('/Users/brunahara/Downloads/Projeto-LTD/ltd-projeto-2/documentos/Taco_4a_edicao_2011.csv')
-print(data.head())
-data.to_json('/Users/brunahara/Downloads/Projeto-LTD/ltd-projeto-2/documentos/Taco_4a_edicao_2011.json')
-json_data = data.to_json()
-print(json_data)
+tabela_taco = pd.read_csv('/Users/brunahara/Downloads/Projeto-LTD/ltd-projeto-2/documentos/Taco_4a_edicao_2011.csv')
+print(tabela_taco)
+tabela_taco.to_json('/Users/brunahara/Downloads/Projeto-LTD/ltd-projeto-2/documentos/Taco_4a_edicao_2011.json')
+json_tabela_taco = tabela_taco.to_json()
+print(json_tabela_taco)
 with open('/Users/brunahara/Downloads/Projeto-LTD/ltd-projeto-2/documentos/Taco_4a_edicao_2011.json', 'w') as file:
-    file.write(json_data)
+    file.write(json_tabela_taco)
